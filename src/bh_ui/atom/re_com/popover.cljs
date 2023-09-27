7db6f09c258404b9b90bf08002f2ac5cff14f14f
abowden21 <andrew@ebowden.com>
2023-09-27 16:48:19 -0600

    Consolidated clickable and hoverable popover into one component in popover.cljs

--- src/bh_ui/atom/re_com/popover.cljs
+++ src/bh_ui/atom/re_com/popover.cljs
@@ -8,15 +8,16 @@
 
 (log/info "bh-ui.atom.re-com.popover")
 
-(defn clickable-popover
-  "A popover that is toggled by clicking the component
+(defn popover-wrapper
+  "A popover that is toggled by clicking the component or hovering over the component
 
    data Map (required)
     :component (required) Hiccup - the component that the popover is attached to
 
-   config - Map
-    :title (optional) String - title of popover Ex: \"Title\"
-    :body-text (optional) String - body text of popover Ex: \"Body\"
+   config - Map (optional)
+    :clickable (optional) boolean - when true the popover is activated by clicking, when false popover is activated by hovering
+    :popover-title (optional) String - title of popover Ex: \"Title\"
+    :popover-body-text (optional) String - body text of popover Ex: \"Body\"
     :position (optional) keyword - position of the popover relative to component Ex: \":above-left\", \":below-center\""
   [& {:keys [data config]}]
   (let [cfg           (h/resolve-value config)
@@ -26,45 +27,22 @@
      [rc/popover-anchor-wrapper :src (rc/at)
       :showing? showing?
       :position (or (:position @cfg) :above-center)
-      :anchor [:div {:on-click #(swap! showing? not)}
+      :anchor [:div
+               (if (not (= nil config))
+                 (if (:clickable @cfg)
+                   {:on-click #(swap! showing? not)}
+                   {:on-mouse-enter #(reset! showing? true)
+                    :on-mouse-leave #(reset! showing? false)})
+                 {})
                (:component @d)]
       :popover [rc/popover-content-wrapper :src (rc/at)
-                :title (:title @cfg)
+                :title (:popover-title @cfg)
                 :no-clip? true
-                :body [layout/markdown-block (:body-text @cfg)]]]]))
-
-(defn hover-popover
-
-  "A popover that is toggled by hovering over the component
-
-  data Map (required)
-   :component (required) Hiccup - the component that the popover is attached to
-
-  config - Map
-    :title (optional) String - title of popover Ex: \"Title\"
-    :body-text (optional) String - body text of popover Ex: \"Body\"
-    :position (optional) keyword - position of the popover relative to component Ex: \":above-left\", \":below-center\""
-  [& {:keys [data config]}]
-  (let [cfg           (h/resolve-value config)
-        d             (h/resolve-value data)
-        showing? (r/atom false)]
-    [:div
-     [rc/popover-anchor-wrapper :src (rc/at)
-      :showing? showing?
-      :position (or (:position @cfg) :above-center)
-      :anchor [:div {:on-mouse-enter #(reset! showing? true)
-                     :on-mouse-leave #(reset! showing? false)}
-               (:component @d)]
-      :popover [rc/popover-content-wrapper :src (rc/at)
-                :title (:title @cfg)
-                :no-clip? true
-                :close-button? false
-                :body [layout/markdown-block (:body-text @cfg)]]]]))
+                :close-button? (or (:clickable @cfg) false)
+                :body [layout/markdown-block (:popover-body-text @cfg)]]]]))
 
 
-
-
-(def element-def {:rc/clickable-popover {:component clickable-popover :child :keyword}})
+(def element-def {:rc/clickable-popover {:component popover-wrapper :child :keyword}})
 
 
 (re-frame.core/dispatch-sync [:register-meta element-def])
