29eb54b6a609ac6147e2bd313942e02012a95fdf
Chris Wasser <cawasser@mac.com>
2023-11-12 22:15:03 -0500

    - using text (GeographicText) as a stand-in for the Collada model until we sort out the (very, very stupid) WW design for async loading of the 3D model data using nasty stateful side-effects.

--- src/bh_ui/atom/worldwind/globe/models.cljs
+++ src/bh_ui/atom/worldwind/globe/models.cljs
@@ -1,6 +1,23 @@
-(ns bh-ui.atom.worldwind.globe.models)
+(ns bh-ui.atom.worldwind.globe.models
+  (:require ["worldwindjs" :as WorldWind]
+            [bh-ui.atom.worldwind.globe.shape :as shape]
+            [bh-ui.atom.worldwind.globe.location :as location]
+            [bh-ui.atom.worldwind.globe.shape.attributes :as attributes]
+            [taoensso.timbre :as log]))
 
 
 (def model-format "collada")
+
+
+(defmethod shape/make-shape :shape/model [{:keys [id model-folder url position]}]
+  (let [label      (WorldWind/GeographicText. (location/position position) id)
+        attributes (attributes/text-attributes
+                     {:fill-color    [1 0.9 0.0 1.0]
+                      :outline-color [1 0.9 0.0 1.0]
+                      :width         1})]
+    (set! (.-attributes label) attributes)
+    label))
+
+
 
 
