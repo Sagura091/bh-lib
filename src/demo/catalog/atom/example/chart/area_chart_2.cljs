4ef80cfdb9a61ec7f09b96f195e72f78345287b3
Chris Wasser <cawasser@mac.com>
2023-11-20 12:59:17 -0500

    - config examples for area-chart-2 (now to refactor the support into a separate namespace, so we can fix all the other Recharts types)

--- src/demo/catalog/atom/example/chart/area_chart_2.cljs
+++ src/demo/catalog/atom/example/chart/area_chart_2.cljs
@@ -151,8 +151,101 @@
                                               :else ())))]]]]]))
 
 
-(defn- config-tools [config reset-config]
-  [:p "config-tools"])
+(defn- config-tools [data config reset-config]
+  (log/info "config-tools (a)" config)
+
+  (let [cat-config (categorize-item config)
+        old-config (condp = cat-config
+                     :subscription (bh/utils-subscribe-local config)
+                     :ratom @config
+                     :else config)]
+
+    (log/info "config-tools (b)" config
+      "_____" cat-config "_____" old-config)
+
+    [rc/v-box :src (rc/at)
+     :gap "3px"
+     :width "100%"
+     :height "30%"
+     :children [[sd/show-config config]
+                [rc/h-box :src (rc/at)
+                 :gap "10px"
+                 :class "tools-panel"
+                 :children [[:label.h5 "Config Properties:"]
+                            [rc/button :label "Default" :on-click #(condp = cat-config
+                                                                     :subscription (bh/utils-handle-change-path config
+                                                                                     [[bh/utils-set-local-values [] reset-config]])
+                                                                     :ratom (reset! config reset-config)
+                                                                     :else ())]
+                            [rc/button :label "! brush" :on-click #(condp = cat-config
+                                                                     :subscription (bh/utils-handle-change-path config
+                                                                                     [[update-in [:brush] not]])
+                                                                     :ratom (swap! config update-in [:brush] not)
+                                                                     :else ())]
+                            [rc/button :label "! :uv" :on-click #(condp = cat-config
+                                                                   :subscription (bh/utils-handle-change-path config
+                                                                                   [[update-in [:uv :include] not]])
+                                                                   :ratom (swap! config update-in [:uv :include] not)
+                                                                   :else ())]
+                            [rc/button :label "! :pv" :on-click #(condp = cat-config
+                                                                   :subscription (bh/utils-handle-change-path config
+                                                                                   [[update-in [:pv :include] not]])
+                                                                   :ratom (swap! config update-in [:pv :include] not)
+                                                                   :else ())]
+                            [rc/button :label "! :tv" :on-click #(condp = cat-config
+                                                                   :subscription (bh/utils-handle-change-path config
+                                                                                   [[update-in [:tv :include] not]])
+                                                                   :ratom (swap! config update-in [:tv :include] not)
+                                                                   :else ())]
+                            [rc/button :label "! :amt" :on-click #(condp = cat-config
+                                                                    :subscription (bh/utils-handle-change-path config
+                                                                                    [[update-in [:amt :include] not]])
+                                                                    :ratom (swap! config update-in [:amt :include] not)
+                                                                    :else ())]
+                            [bh/chart-utils-color-config config ":uv :fill" [:uv :fill] :above-center]
+                            [bh/chart-utils-color-config config ":tv :fill" [:tv :fill] :above-center]
+                            [bh/chart-utils-color-config config ":pv :fill" [:pv :fill] :above-center]
+                            [bh/chart-utils-color-config config ":amt :fill" [:amt :fill] :above-center]
+                            [rc/button :label "stack uv/pv" :on-click #(condp = cat-config
+                                                                         :subscription (do
+                                                                                         (bh/utils-handle-change-path config
+                                                                                           [[assoc-in [:uv :stackId] "b"]])
+                                                                                         (bh/utils-handle-change-path config
+                                                                                           [[assoc-in [:pv :stackId] "b"]]))
+                                                                         :ratom (reset! config (-> @config
+                                                                                                 (assoc-in [:uv :stackId] "a")
+                                                                                                 (assoc-in [:pv :stackId] "a")))
+                                                                         :else ())]
+                            [rc/button :label "unstack uv/pv" :on-click #(condp = cat-config
+                                                                           :subscription (do
+                                                                                           (bh/utils-handle-change-path config
+                                                                                             [[assoc-in [:uv :stackId] ""]])
+                                                                                           (bh/utils-handle-change-path config
+                                                                                             [[assoc-in [:pv :stackId] ""]]))
+                                                                           :ratom (reset! config (-> @config
+                                                                                                   (assoc-in [:uv :stackId] "")
+                                                                                                   (assoc-in [:pv :stackId] "")))
+                                                                           :else ())]
+                            [rc/button :label "stack tv/amt" :on-click #(condp = cat-config
+                                                                          :subscription (do
+                                                                                          (bh/utils-handle-change-path config
+                                                                                            [[assoc-in [:tv :stackId] "b"]])
+                                                                                          (bh/utils-handle-change-path config
+                                                                                            [[assoc-in [:amt :stackId] "b"]]))
+                                                                          :ratom (reset! config (-> @config
+                                                                                                  (assoc-in [:tv :stackId] "b")
+                                                                                                  (assoc-in [:amt :stackId] "b")))
+                                                                          :else ())]
+                            [rc/button :label "unstack tv/amt" :on-click #(condp = cat-config
+                                                                            :subscription (do
+                                                                                            (bh/utils-handle-change-path config
+                                                                                              [[assoc-in [:tv :stackId] ""]])
+                                                                                            (bh/utils-handle-change-path config
+                                                                                              [[assoc-in [:amt :stackId] ""]]))
+                                                                            :ratom (reset! config (-> @config
+                                                                                                    (assoc-in [:tv :stackId] "")
+                                                                                                    (assoc-in [:amt :stackId] "")))
+                                                                            :else ())]]]]]))
 
 
 (def example-data data/meta-tabular-data)
@@ -162,6 +255,87 @@
 (defn chart-container [chart]
   [:div.component-example {:style {:width "100%" :height "450px"}}
    chart])
+
+
+(defn config-ratom-example []
+  (let [component-id "area-chart-2"
+        container-id ""
+        config       (r/atom example-config)]
+
+    (acu/demo "Area Chart 2"
+      "Trying to fix Recharts implementation (config is a `(r)atom`)"
+      [rc/box :src (rc/at)
+       :justify :center
+       :width "100%"
+       :height "100%"
+       :child [rc/v-box :src (rc/at)
+               :gap "3px"
+               :width "100%"
+               :height "100%"
+               :children [[chart-container
+                           [area-chart/component
+                            :data example-data
+                            :config config
+                            :component-id component-id
+                            :container-id container-id]]
+                          [config-tools example-data config example-config]]]]
+      area-chart/source-code)))
+
+
+(defn config-struct-example []
+  (let [component-id "area-chart-2"
+        container-id ""]
+
+    (acu/demo "Area Chart 2"
+      "Trying to fix Recharts implementation (config is a *plain* hash-map)"
+      [rc/box :src (rc/at)
+       :justify :center
+       :width "100%"
+       :height "100%"
+       :child [rc/v-box :src (rc/at)
+               :gap "3px"
+               :width "100%"
+               :height "100%"
+               :children [[chart-container
+                           [area-chart/component
+                            :data example-data
+                            :config example-config
+                            :component-id component-id
+                            :container-id container-id]]]]]
+      area-chart/source-code)))
+
+
+(defn config-sub-example []
+  (let [component-id :area-chart-2-data-sub-demo
+        container-id ""
+        config       [component-id :blackboard :config]
+        id           (atom nil)]
+
+    (fn []
+      (when (nil? @id)
+        (reset! id component-id)
+        (bh/utils-init-container-locals @id {:blackboard {:config example-config}
+                                             :container  container-id})
+        (bh/utils-dispatch-local @id [:container] component-id))
+
+      (acu/demo "Area Chart 2"
+        "Trying to fix Recharts implementation (subscribing to `:config`)"
+        [rc/box :src (rc/at)
+         :justify :center
+         :width "100%"
+         :height "100%"
+         :child [rc/v-box :src (rc/at)
+                 :gap "3px"
+                 :width "100%"
+                 :height "100%"
+                 :children [[chart-container
+                             [area-chart/component
+                              :data example-data
+                              :config config
+                              :component-id component-id
+                              :container-id container-id]]
+                            [config-tools example-data config example-config]]]]
+        area-chart/source-code))))
 
 
 (defn data-ratom-example []
@@ -187,6 +361,29 @@
                           [data-tools data example-config
                            data/meta-tabular-data
                            data/random-meta-tabular-data]]]]
+      area-chart/source-code)))
+
+
+(defn data-struct-example []
+  (let [component-id "area-chart-2"
+        container-id ""
+        data         example-data]
+
+    (acu/demo "Area Chart 2"
+      "Trying to fix Recharts implementation (data is a *plain* hash-map)"
+      [rc/box :src (rc/at)
+       :justify :center
+       :width "100%"
+       :height "100%"
+       :child [rc/v-box :src (rc/at)
+               :gap "3px"
+               :width "100%"
+               :height "100%"
+               :children [[chart-container
+                           [area-chart/component
+                            :data data
+                            :component-id component-id
+                            :container-id container-id]]]]]
       area-chart/source-code)))
 
 
@@ -228,8 +425,12 @@
 
 
 (defn examples []
-  [me/examples {"data-ratom" [data-ratom-example]
-                "data-sub"   [data-sub-example]}])
+  [me/examples {"data-ratom"    [data-ratom-example]
+                "data-struct"   [data-struct-example]
+                "data-sub"      [data-sub-example]
+                "config-ratom"  [config-ratom-example]
+                "config-struct" [config-struct-example]
+                "config-sub"    [config-sub-example]}])
 
 
 (comment
