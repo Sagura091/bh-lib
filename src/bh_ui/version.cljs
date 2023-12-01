227f916d2f1d1fa067fae660ec9347e88f59aabd
Chris Wasser <cawasser@mac.com>
2023-12-01 15:11:46 -0500

    Bump bh-lib to 3.2.1 and spit metadata

--- src/bh_ui/version.cljs
+++ src/bh_ui/version.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "bh-lib")
 (def path ".")
-(def version "3.2.0")
-(def tag "v3.2.0")
-(def generated-at "2023-11-01T20:12:01Z")
+(def version "3.2.1")
+(def tag "v3.2.1")
+(def generated-at "2023-12-01T20:11:46Z")
